2c17175ebf44a283d64dca3d361a61bfded3909c
SamuelOmodele <samuelomodele19@gmail.com>
2026-09-17 12:52:53 +0100

    updated hero button width for mobile

--- components/LandingPage/HeroSection.tsx
+++ components/LandingPage/HeroSection.tsx
@@ -62,10 +62,10 @@ const HeroSection = () => {
               variants={itemVariants}
               className="mt-7 flex flex-row items-center max-lg:justify-center gap-3"
             >
-              <button className="px-7 sm:px-10 py-2.5 cursor-pointer font-plus-jarkata-sans font-medium text-[#1A202C] text-sm rounded-[5px] bg-[#FFC909] hover:bg-[#FFC909]/80 duration-150 w-auto transition-transform hover:scale-105">
+              <button className="px-7 sm:px-10 py-2.5 cursor-pointer font-plus-jarkata-sans font-medium text-[#1A202C] text-sm rounded-[5px] bg-[#FFC909] hover:bg-[#FFC909]/80 duration-150 w-40 sm:w-auto transition-transform hover:scale-105">
                 Order a Ride
               </button>
-              <button className="px-5 sm:px-7 py-2.5 cursor-pointer font-plus-jarkata-sans font-medium text-[#1A202C] text-sm rounded-[5px] bg-white/30 hover:bg-white/50 sm:bg-white/20 sm:hover:bg-white/40 duration-150 w-auto transition-transform hover:scale-105">
+              <button className="px-5 sm:px-7 py-2.5 cursor-pointer font-plus-jarkata-sans font-medium text-[#1A202C] text-sm rounded-[5px] bg-white/30 hover:bg-white/50 sm:bg-white/20 sm:hover:bg-white/40 duration-150 w-40 sm:w-auto transition-transform hover:scale-105">
                 Become a Driver
               </button>
             </motion.div>
